28e731cdc433ca527b05d6c5bc0db521fe03da08
abhishek-end <abhishekyadav49534@gmail.com>
2024-07-05 00:36:47 +0530

    addig video with gsap

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//js for swipper slider auto in 3 sec
 var swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   spaceBetween: 30,
@@ -6,4 +7,26 @@ var swiper = new Swiper(".swiper-container", {
     delay: 3000,
     disableOnInteraction: false,
   },
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".section").forEach(function (page) {
+  let video = page.querySelectorAll("video");
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: page,
+      scrub: true,
+      pin: false,
+    },
+  });
+  tl.from(video, {
+    yPercent: -100,
+    ease: "all",
+    
+  }).to(video, {
+    yPercent: 50,
+    ease: "all",
+  });
 });
