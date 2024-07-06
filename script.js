78c55b7d8d61b8d08c8adfd2bdcb1038eafbed03
abhishek-end <abhishekyadav49534@gmail.com>
2024-07-06 19:37:44 +0530

    complete UI changed after 2 hard try hamburger  menu created 😉

--- script.js
+++ script.js
@@ -1,11 +1,66 @@
-//js for swipper slider auto in 3 sec
-var swiper = new Swiper(".swiper-container", {
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".border");
+const menuText = document.querySelector(".navigation h1");
+let menuOpen = false;
+
+hamburger.addEventListener("click" || "mouseenter", () => {
+  const spans = hamburger.querySelectorAll("span");
+  const tl = gsap.timeline();
+  if (!menuOpen) {
+    tl.to(menu, {
+      height: "80vh",
+      opacity: 1,
+      duration: 0.6,
+      ease: "power3.inOut",
+    });
+    gsap.to(spans[0], {
+      y: 6,
+      rotation: 60,
+      backgroundColor: "#fff",
+      duration: "0.6",
+      ease: "power3.inOut",
+    });
+    gsap.to(spans[1], {
+      y: -6,
+      rotation: -60,
+      backgroundColor: "#fff",
+      duration: "0.3",
+      ease: "power3.inOut",
+    });
+    hamburger.classList.add("active");
+    menuText.textContent = "Close";
+    menuOpen = true;
+  } else {
+    tl.to(menu, {
+      height: "0vh",
+      opacity: 0,
+      duration: 0.5,
+      ease: "power3.inOut",
+    });
+    gsap.to(spans[0], {
+      y: 0,
+      rotation: 0,
+      backgroundColor: "#fff",
+      duration: 0.3,
+    });
+    gsap.to(spans[1], {
+      y: 0,
+      rotation: 0,
+      backgroundColor: "#fff",
+      duration: 0.6,
+    });
+    hamburger.classList.remove("active");
+    menuText.textContent = "Menu";
+    menuOpen = false;
+  }
+});
+
+let swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
-  spaceBetween: 10,
   loop: true,
+  spaceBetween: 10,
   autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
+    delay: 2500,
   },
 });
 
